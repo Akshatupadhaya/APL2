52f834b48b53b2a7639c419865f1093378565e51
241B025 <241B025@juet.co.in>
2026-08-17 15:27:41 +0530

    Add Java programs

---
+++ Main.java
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+//public class Main
+//{
+ //public static void main(String[] args) {
+		//Add a1 = new Add();// craeting object of class add 
+		//int result = a1.sum(3,5);
+		//System.out.println("this is the summ:"+result);
+//	}
+//}
+public class Main {
+    public static void main(String[] args) {
+        int num = 2;
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num + " x " + i + " = " + (num * i));
+        }
+    }
+}
